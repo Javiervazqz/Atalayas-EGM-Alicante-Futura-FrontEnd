c7ea2a67f654abdbe8a0fc8039d5088dd0f0b552
SergioGaliana <114169970+SergioGaliana@users.noreply.github.com>
2026-03-09 14:52:30 +0100

    First commit, login y registro

---
+++ atalayas/app/index.tsx
@@ -0,0 +1,100 @@
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { router } from "expo-router";
+
+export default function Welcome() {
+  return (
+    <View style={styles.container}>
+
+      <Text style={styles.title}>BIENVENIDO</Text>
+
+      <View style={styles.logoBox}>
+        <View style={styles.logoCircle}>
+          <Text style={styles.logoText}>DIGI</Text>
+        </View>
+      </View>
+
+      <View style={styles.card}>
+
+        <Text style={styles.text}>Si eres alguien nuevo</Text>
+
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={() => router.push("/registro")}
+        >
+          <Text>Regístrate</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.text}>Si ya tienes una cuenta</Text>
+
+        <TouchableOpacity 
+          style={styles.button}
+          onPress={() => router.push("/login")}
+        >
+          <Text>Inicia sesión</Text>
+        </TouchableOpacity>
+
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: "#d7b56d",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  title: {
+    fontSize: 24,
+    color: "white",
+    marginBottom: 20,
+  },
+
+  logoBox: {
+    backgroundColor: "white",
+    padding: 20,
+    borderRadius: 20,
+    marginBottom: 30,
+  },
+
+  logoCircle: {
+    width: 80,
+    height: 80,
+    borderRadius: 40,
+    backgroundColor: "black",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  logoText: {
+    color: "yellow",
+    fontWeight: "bold",
+  },
+
+  card: {
+    backgroundColor: "#e8e8e8",
+    padding: 25,
+    borderRadius: 20,
+    alignItems: "center",
+    width: 250,
+  },
+
+  text: {
+    marginTop: 10,
+  },
+
+  button: {
+    marginVertical: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    borderWidth: 1,
+    borderColor: "#555",
+    backgroundColor: "white",
+  },
+
+});
---
+++ atalayas/app/registro.tsx
@@ -0,0 +1,123 @@
+import { View, Text, StyleSheet, TextInput, TouchableOpacity } from "react-native";
+import { useState } from "react";
+import { router } from "expo-router";
+
+
+export default function Registro() {
+
+  const [rol, setRol] = useState("");
+
+  return (
+    <View style={styles.container}>
+
+      <Text style={styles.title}>Registro</Text>
+
+      <View style={styles.card}>
+
+        <TextInput placeholder="Nombre" style={styles.input} />
+        <TextInput placeholder="Email" style={styles.input} />
+        <TextInput placeholder="Contraseña" secureTextEntry style={styles.input} />
+
+        <Text style={styles.label}>Tipo de rol</Text>
+
+        <View style={styles.roleContainer}>
+          <TouchableOpacity
+            style={styles.roleButton}
+            onPress={() => setRol("empleado")}
+          >
+            <Text>Empleado</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.roleButton}
+            onPress={() => setRol("empresa")}
+          >
+            <Text>Empresa</Text>
+          </TouchableOpacity>
+        </View>
+
+
+        {rol === "empleado" && (
+          <TextInput
+            placeholder="Buscar empresa"
+            style={styles.input}
+          />
+        )}
+
+        {rol === "empresa" && (
+          <TextInput
+            placeholder="Nombre de la empresa"
+            style={styles.input}
+          />
+        )}
+        <TouchableOpacity 
+            style={styles.button}
+            onPress={() => router.push("/(tabs)")}>
+            <Text>Registrarse</Text>
+        </TouchableOpacity>
+
+      </View>
+
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+
+  container: {
+    flex: 1,
+    backgroundColor: "#d7b56d",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  title: {
+    fontSize: 24,
+    color: "white",
+    marginBottom: 20,
+  },
+
+  card: {
+    backgroundColor: "#e8e8e8",
+    padding: 25,
+    borderRadius: 20,
+    width: 280,
+  },
+
+  label: {
+    marginTop: 10,
+    marginBottom: 5,
+  },
+
+  input: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
+    marginVertical: 5,
+  },
+
+  roleContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10,
+  },
+
+  roleButton: {
+    backgroundColor: "white",
+    padding: 10,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#555",
+    width: "48%",
+    alignItems: "center",
+  },
+
+  button: {
+    marginTop: 15,
+    padding: 12,
+    backgroundColor: "white",
+    borderRadius: 20,
+    alignItems: "center",
+    borderWidth: 1,
+  }
+
+});
