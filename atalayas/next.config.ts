b169c049be78253edcb645fb467343331e19ca65
Javiervazqz <javvazgar@alu.edu.gva.es>
2026-03-18 13:25:25 +0100

    pantalla de empleados sin boton de editar

--- atalayas/next.config.ts
+++ atalayas/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/', // Cuando el usuario entra a la raíz (localhost:5173)
+        destination: '/login', // Lo mandamos directo al login
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
